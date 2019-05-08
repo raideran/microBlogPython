3d7374a4995c55cd8bc1b82c270fe6e910a3d027
Jose Porras <japslatino@gmail.com>
2019-05-08 14:05:40 -0600

    Adding 'For' to template with example posts

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'user': 'Jose'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Jose'}
+    posts = [
+        {
+            'author': {'username': 'Jhon'},
+            'body': 'Beautiful day in Alajuelita'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'EndGame Movie was amazing'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
