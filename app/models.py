755d70a9741012c5ef44e29724ca5bc19941729e
Jose Porras <japslatino@gmail.com>
2019-05-13 13:53:06 -0600

    Adding password hash to user model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -7,6 +8,17 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email, 
+        self.set_password(password) 
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
