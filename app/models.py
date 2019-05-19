5087c4a334184eeae0fb4d62ae5df86b912dc130
Jose Porras <japslatino@gmail.com>
2019-05-18 20:52:31 -0600

    Added sub templates and posts avatars

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -20,6 +21,10 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password, password)
+    
+    def avatar(self, size):
+        digest=  md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?s={}'.format(digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
